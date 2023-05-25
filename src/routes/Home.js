f19ccd86eb1ebbd67031d0c56162bbb9e9aa8061
jwzzang12 <moonbb12@gmail.com>
2023-05-26 00:30:02 +0100

    Link database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,37 @@
+import { useState } from 'react';
+import { firestore } from '../fbase';
+import { addDoc, collection } from 'firebase/firestore';
+
 export default function Home() {
-  return <span>Home</span>;
+  const [sweet, setSweet] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(firestore, 'sweets'), {
+        sweet,
+        createdAt: Date.now(),
+      });
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+    setSweet('');
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={sweet} onChange={onChange} type='text' placeholder="What's happening?" maxLength={140} />
+        <input type='submit' value='Sweet' />
+      </form>
+    </div>
+  );
 }
