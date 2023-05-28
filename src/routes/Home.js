d5b7358d63ec2b3565bb070e910589a74ff0ba07
jwzzang12 <moonbb12@gmail.com>
2023-05-28 01:40:55 +0100

    Add upload img func

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
-import { firestore } from '../fbase';
+import { firestore, storage } from '../fbase';
 import { addDoc, collection, onSnapshot } from 'firebase/firestore';
 import Sweet from '../components/Sweet';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Home({ userObj }) {
   const [sweet, setSweet] = useState('');
   const [sweets, setSweets] = useState([]);
+  const [image, setImage] = useState('');
 
   useEffect(() => {
     const q = collection(firestore, 'sweets');
@@ -20,16 +23,29 @@ export default function Home({ userObj }) {
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    let imgUrl = '';
+
     try {
-      await addDoc(collection(firestore, 'sweets'), {
+      const imgRef = ref(storage, `${userObj.uid}/${uuidv4()}`);
+      const response = await uploadString(imgRef, image, 'data_url');
+      if (imgRef !== '') {
+        imgUrl = await getDownloadURL(response.ref);
+      }
+
+      const sweetObj = {
         text: sweet,
         createdAt: Date.now(),
         creatorId: userObj.uid,
-      });
+        imgUrl,
+      };
+
+      await addDoc(collection(firestore, 'sweets'), sweetObj);
     } catch (error) {
-      console.error('Error adding document: ', error);
+      console.error('Error adding file: ', error);
     }
+
     setSweet('');
+    setImage('');
   };
 
   const onChange = (event) => {
@@ -39,11 +55,37 @@ export default function Home({ userObj }) {
     setSweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const imgFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setImage(result);
+    };
+    reader.readAsDataURL(imgFile);
+  };
+
+  const onClearImage = () => {
+    setImage(null);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={sweet} onChange={onChange} type='text' placeholder="What's happening?" maxLength={140} />
+        <input type='file' accept='image/*' onChange={onFileChange} />
         <input type='submit' value='Sweet' />
+        {image && (
+          <div>
+            <img src={image} alt='' width='50px' height='50px' />
+            <button onClick={onClearImage}>X</button>
+          </div>
+        )}
       </form>
       <div>
         {sweets.map((item) => {
