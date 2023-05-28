cc58b20cfbdc556b7bac9be18c4c3cd44647bbd1
jwzzang12 <moonbb12@gmail.com>
2023-05-28 23:57:10 +0100

    Delete img func

--- src/components/Sweet.js
+++ src/components/Sweet.js
@@ -1,13 +1,20 @@
 import { deleteDoc, doc } from 'firebase/firestore';
-import { firestore } from '../fbase';
+import { firestore, storage } from '../fbase';
+import { deleteObject, ref } from 'firebase/storage';
 
 export default function Sweet({ sweetObj, isOwner }) {
   const onDeleteClick = async () => {
     const ok = window.confirm('Are you sure you want to delete this Sweet?');
     const sweetTextRef = doc(firestore, 'sweets', `${sweetObj.id}`);
-
     if (ok) {
-      await deleteDoc(sweetTextRef);
+      try {
+        await deleteDoc(sweetTextRef);
+        if (sweetObj.attachmentUrl !== '') {
+          await deleteObject(ref(storage, sweetObj.attachmentUrl));
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
